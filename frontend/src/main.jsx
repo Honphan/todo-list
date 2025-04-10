f418314b5f4a5d1eb8c30245cee27629f3328e29
Hon phan <honphan194@gmail.com>
2025-04-10 17:37:24 +0700

    update-router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} /> 
+    </Routes>
+</Router>
 )
